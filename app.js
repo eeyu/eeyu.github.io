46445902bcde65de03c7ca452f88a9234451a9c1
eeyu <evanyu134@gmail.com>
2021-08-09 17:55:24 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,19 +1,19 @@
 'use strict'
 
-const switcher = document.querySelector('.btn');
+// const switcher = document.querySelector('.btn');
 
-switcher.addEventListener('click', function() {
-    document.body.classList.toggle('dark-theme')
+// switcher.addEventListener('click', function() {
+//     document.body.classList.toggle('dark-theme')
 
-    var className = document.body.className;
-    if(className == "dark-theme") {
-        this.textContent = "Dark";
-    }
-    else {
-        this.textContent = "Light";
-    }
-    console.log('current class name: ' + className);
-});
+//     var className = document.body.className;
+//     if(className == "dark-theme") {
+//         this.textContent = "Dark";
+//     }
+//     else {
+//         this.textContent = "Light";
+//     }
+//     console.log('current class name: ' + className);
+// });
 
 // document.addEventListener('keydown', (event) => {
 //     console.log("evernt occrred");
