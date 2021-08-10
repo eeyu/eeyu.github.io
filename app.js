e9d2d0cbabdd1f66335d6066ef62ca524297c2e8
eeyu <evanyu134@gmail.com>
2021-08-09 18:01:43 -0700

    Update app.js

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 'use strict'
 
-// const switcher = document.querySelector('.btn');
+const switcher = document.querySelector('.btn');
 
-// switcher.addEventListener('click', function() {
-//     document.body.classList.toggle('dark-theme')
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme')
 
-//     var className = document.body.className;
-//     if(className == "dark-theme") {
-//         this.textContent = "Dark";
-//     }
-//     else {
-//         this.textContent = "Light";
-//     }
-//     console.log('current class name: ' + className);
-// });
+    var className = document.body.className;
+    if(className == "dark-theme") {
+        this.textContent = "Dark";
+    }
+    else {
+        this.textContent = "Light";
+    }
+    console.log('current class name: ' + className);
+});
 
-// document.addEventListener('keydown', (event) => {
-//     console.log("evernt occrred");
-//     var name = event.key;
-//     var code = event.code;
-//     if (name === 'Control') {
-//       // Do nothing.
-//       return;
-//     }
-//     if (event.ctrlKey) {
-//         console.log(`Combination of ctrlKey + ${name} \n Key code Value: ${code}`);
-//     } else {
-//         console.log(`Key pressed ${name} \n Key code Value: ${code}`);
-//     }
-//   }, false);
+document.addEventListener('keydown', (event) => {
+    console.log("evernt occrred");
+    var name = event.key;
+    var code = event.code;
+    if (name === 'Control') {
+      // Do nothing.
+      return;
+    }
+    if (event.ctrlKey) {
+        console.log(`Combination of ctrlKey + ${name} \n Key code Value: ${code}`);
+    } else {
+        console.log(`Key pressed ${name} \n Key code Value: ${code}`);
+    }
+}, false);
 
